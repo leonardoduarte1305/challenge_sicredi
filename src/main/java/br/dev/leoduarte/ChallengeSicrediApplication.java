90c1dd13a15c440b16120244eb28e4cf0b58f97a
Leonardo Duarte <leonardoduarte1305@gmail.com>
2022-02-25 09:27:29 -0300

    Projeto criado e enviado ao Github

---
+++ src/main/java/br/dev/leoduarte/ChallengeSicrediApplication.java
@@ -0,0 +1,13 @@
+package br.dev.leoduarte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeSicrediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeSicrediApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/leoduarte/ChallengeSicrediApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.leoduarte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeSicrediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
