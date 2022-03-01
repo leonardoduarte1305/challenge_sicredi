d58715168cb440aba2f3644d649031496daf69b6
Leonardo Duarte <leonardoduarte1305@gmail.com>
2022-03-01 16:00:48 -0300

    Organizado o dockerfile para criacao da imagem, refeitos alguns detalhes em testes

--- src/test/java/br/dev/leoduarte/sicredi/utils/FormatarDataTest.java
+++ src/test/java/br/dev/leoduarte/sicredi/utils/FormatarDataTest.java
@@ -1,16 +1,15 @@
 package br.dev.leoduarte.sicredi.utils;
 
-import static org.junit.Assert.assertTrue;
-
 import java.time.LocalDateTime;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 public class FormatarDataTest {
 
 	@Test
 	void retornaOkSeFormatarCorretamenteAData() {
-		assertTrue(new FormatarData().formatar(LocalDateTime.now())
+		Assert.assertTrue(new FormatarData().formatar(LocalDateTime.now())
 				.matches("\\d{2}\\/\\d{2}\\/\\d{4}\\ \\d{2}\\:\\d{2}\\:\\d{2}"));
 	}
 
